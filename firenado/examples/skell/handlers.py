a774affc608078bc92208790c770b108be4e09c4
Flavio Garcia <piraz@candango.org>
2015-08-02 15:36:41 -0400

    Sessison handler is using session. Handlers are using templates.
    The session handler is using the session object to persist values
    properly and to reset the value.
    
    Handlers are using templates instead of writing strings to the
    stream.

--- firenado/examples/skell/handlers.py
+++ firenado/examples/skell/handlers.py
@@ -4,15 +4,19 @@ import firenado.core
 class IndexHandler(firenado.core.TornadoHandler):
 
     def get(self):
-        self.write("IndexHandler output")
+        self.render("index.html", message="Hello world!!!")
 
 class SessionHandler(firenado.core.TornadoHandler):
 
     def get(self):
+        reset = details=self.get_argument("reset", False, True)
+        if reset:
+            self.session.delete('counter')
+            self.redirect('/session')
+            return None
         counter = 0
         if self.session.has('counter'):
             counter = self.session.get('counter')
         counter += 1
         self.session.set('counter', counter)
-        self.write("Counter: %i" % counter)
-        #self.render("index.html", initial_message="Hello world!!!")
+        self.render("session.html", session_value=counter)
