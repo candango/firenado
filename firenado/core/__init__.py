46794b214d0ce5367fd79c254b353d54e592ae7f
Flavio Garcia <piraz@candango.org>
2016-01-27 23:55:33 -0500

    Removed vim comment.

--- firenado/core/__init__.py
+++ firenado/core/__init__.py
@@ -13,8 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
 
 from __future__ import (absolute_import, division, print_function,
                         with_statement)
