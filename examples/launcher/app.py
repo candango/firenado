e6d95da34b5ce77a7b782bc931ae33219992a7ad
Flavio Garcia <piraz@candango.org>
2018-12-16 23:09:26 -0500

    Used ProcessLauncher to launch the charge app.
    Refs: #249

--- examples/launcher/app.py
+++ examples/launcher/app.py
@@ -1,6 +1,7 @@
 from launcher import handlers
 from firenado import tornadoweb
-from firenado.tornadoweb import FirenadoLauncher
+from firenado.launcher import ProcessLauncher
+from tornado import gen
 import os
 
 
@@ -10,16 +11,18 @@ class LauncherComponent(tornadoweb.TornadoComponent):
         super(LauncherComponent, self).__init__(name, application)
         self.launcher_path = os.path.abspath(os.path.dirname(__file__))
         self.charge_path = os.path.join(self.launcher_path, "charge")
+        self.launcher = None
 
     def get_handlers(self):
         return [
             (r'/', handlers.IndexHandler),
         ]
-
+    @gen.coroutine
     def initialize(self):
-        firenado_launcher = FirenadoLauncher(dir=self.charge_path)
-        #firenado_launcher.load()
-        #firenado_launcher.launch()
+        self.launcher = ProcessLauncher(dir=self.charge_path)
+        self.launcher.load()
+        yield self.launcher.launch()
+        print(self.launcher.process.isalive())
 
     def shutdown(self):
-        print("Buga")
+        self.launcher.shutdown()
