dafbbfd07819723c2919d466a5abdb3cf169e718
Flavio Garcia <piraz@candango.org>
2020-02-22 12:12:16 -0500

    Added python 3.8 classifier and test configuration.

--- setup.py
+++ setup.py
@@ -77,6 +77,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Internet :: WWW/HTTP :: WSGI",
