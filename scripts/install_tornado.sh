a0eb7c77af1039e77215c0099dede0caa5576b91
Flavio Garcia <piraz@candango.org>
2020-03-18 17:26:27 -0400

    Fixed bash script else syntax.
    Fixes: #303

--- scripts/install_tornado.sh
+++ scripts/install_tornado.sh
@@ -7,7 +7,7 @@ PYTHON_VERSION=`python -c 'import sys; print(sys.version_info[0])'`
 if [ $PYTHON_VERSION -eq 2 ]; then
     pip install -r requirements/tornado_legacy.txt;
     exit;
-else:
+else
     pip install -r requirements/tornado_new.txt;
     exit;
 fi
