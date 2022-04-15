5a34a3fd50f9f3fe5e338ded39f12dc0053e8386
Flavio Garcia <piraz@candango.org>
2022-04-15 12:45:32 -0400

    Launching charge application from a callback.
    This will fix the firenado.conf collision and seems the right way
    to do it.
    
    Refs: #402

--- examples/launcher/app.py
+++ examples/launcher/app.py
@@ -1,7 +1,6 @@
 from . import handlers
 from firenado import tornadoweb
 from firenado.launcher import ProcessLauncher
-from tornado import gen
 import os
 
 
@@ -12,20 +11,27 @@ class LauncherComponent(tornadoweb.TornadoComponent):
         self.launcher_path = os.path.abspath(os.path.dirname(__file__))
         self.charge_path = os.path.join(self.launcher_path, "charge")
         self.launcher = None
+        self.launcher_callback = None
 
     def get_handlers(self):
         return [
             (r'/', handlers.IndexHandler),
         ]
 
-    @gen.coroutine
     def initialize(self):
+        import tornado.ioloop
+        self.launcher_callback = tornado.ioloop.IOLoop.current().add_callback(
+            callback=self.launch_charge
+        )
+
+    async def launch_charge(self):
         import sys
+        os.chdir(self.charge_path)
         self.launcher = ProcessLauncher(dir=self.charge_path,
                                         logfile=sys.stderr)
         self.launcher.load()
-        yield self.launcher.launch()
+        await self.launcher.launch()
+        os.chdir(self.launcher_path)
 
-    @gen.coroutine
     def shutdown(self):
         self.launcher.shutdown()
